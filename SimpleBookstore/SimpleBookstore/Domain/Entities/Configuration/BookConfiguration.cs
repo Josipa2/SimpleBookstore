0d237dafd4ed3dbbda0ad60d042d2a24291a6eb2
Josipa Ružić <jr51736@fer.hr>
2025-11-02 23:16:03 +0100

    Added migration for book title configuration

--- SimpleBookstore/SimpleBookstore/Domain/Entities/Configuration/BookConfiguration.cs
+++ SimpleBookstore/SimpleBookstore/Domain/Entities/Configuration/BookConfiguration.cs
@@ -5,7 +5,9 @@ namespace SimpleBookstore.Domain.Entities.Configuration;
 
 public class BookConfiguration : IEntityTypeConfiguration<Book>
 {
-    public void Configure(EntityTypeBuilder<Book> builder) =>
+    public void Configure(EntityTypeBuilder<Book> builder) 
+    {
         builder.Property(a => a.Price).HasPrecision(8, 2).IsRequired();
-
+        builder.HasIndex(x => x.Title).IsUnique();
+    }
 }
